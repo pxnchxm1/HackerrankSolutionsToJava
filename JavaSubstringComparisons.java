349007c82f1a3e6d334a399e5fe71a7f95a7905c
Panchami P Kumar <117977315+pxnchxm1@users.noreply.github.com>
2025-05-20 15:20:39 +0530

    added : 14 - Java Substring Comparisons

---
+++ JavaSubstringComparisons.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaSubstringComparisons {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int n = sc.nextInt();
+        List<String> words = new ArrayList<>();
+        for(int i=0;i<=word.length()-n;i++){
+            words.add(word.substring(i,i+n));
+        }
+        String sm = words.get(0);
+        String gr = words.get(0);
+       
+        for(String s : words){
+            if(sm.compareTo(s)>=0){
+                sm = s;
+            }
+            if(gr.compareTo(s)<0){
+                gr=s;
+            }
+        }
+        
+        System.out.println(sm+"\n"+gr);
+        sc.close();
+    }
+}
