2744a59cbc51580d7e0cba3eb0fe18b6ca9e637e
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-22 14:05:25 +0530

    added:3-StrongPassword

---
+++ ProblemSolving/StrongPassword.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumNumber' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. STRING password
+     */
+
+    public static int minimumNumber(int n, String password) {
+        // Return the minimum number of characters to make the password strong
+
+        int ans=0;
+        if(!password.matches(".*[a-z].*"))ans++;
+        if(!password.matches( ".*[A-Z].*"))ans++;
+        if(!password.matches(".*[0-9].*"))ans++;
+        if(!password.matches(".*[!@#$%^&*()\\-+].*"))ans++;
+        return Math.max(ans,6-n);
+    }
+
+}
+
+public class StrongPassword {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String password = bufferedReader.readLine();
+
+        int answer = Result.minimumNumber(n, password);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
