72ca11b8af91662aff09a10faee7065ab28b8449
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-23 16:48:17 +0530

    added :32-JavaInheritance I

---
+++ Java/JavaInheritanceI.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+}
+public class JavaInheritanceI {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();bird.fly();
+        System.out.println("I am singing");
+    }
+}
+
+
